713e457ca07fa6a1b0f6c7c1d37bd1c9ca123ea7
Sukardi <35399885+vincent12123@users.noreply.github.com>
2023-03-21 21:32:23 +0700

    Update multipage.py

--- multipage.py
+++ multipage.py
@@ -11,19 +11,17 @@ def intro():
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-            forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-            Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+        Halo siswa! Selamat datang di demo aplikasi Streamlit. Di sini, kita akan berkenalan dengan sebuah aplikasi multipage yang berisikan kasir sederhana. 
+       Aplikasi ini sangat berguna bagi kita yang ingin belajar membuat aplikasi dengan bahasa pemrograman 
+       Python dan framework Streamlit.
+       
+       **👈 Silahkan Pilih Demo dari Sidebar** untuk melihat contoh
+        
+        ### Aplikasi ini memungkinkan kita untuk memasukkan data pembelian seperti:
+        - Nama Barang
+        - harga
+        - Jumlah Pembelian
+        ### Untuk Tutorial yang lain silahkan masuk ke [Website saya](sukardinotes.karyabangsa.sch.id)
     """
     )
 
